1190182fd3e6cd50a77e2f4e7e3abad80d6f1656
Ignacio <nachomazzara@gmail.com>
2018-05-26 17:45:04 -0300

    mas pipipipipipipip y mi cabeza va a explotar

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -2,10 +2,25 @@ import * as React from 'react'
 import { IProps, EditorLanguage } from './types'
 import Monaco from 'src/components/Monaco'
 import EditorTabs from './EditorTabs'
+import { debounce } from 'src/modules/common/utils'
 import './Editor.css'
 
-class Editor extends React.PureComponent<IProps> {
+class Editor extends React.PureComponent<IProps, { value: string }> {
   private editor: monaco.editor.IStandaloneCodeEditor
+  private debouncedOnChange: () => void
+
+  constructor(props: any) {
+    super(props)
+    this.state = { value: props.files[props.currentFile].content }
+  }
+
+
+  componentDidMount() {
+    this.debouncedOnChange = debounce(() =>
+      this.props.onChange(
+        this.props.currentFile, this.state.value
+      ), 500)
+  }
 
   handleEditorCreated = (editor: monaco.editor.IStandaloneCodeEditor) => {
     this.editor = editor
@@ -15,40 +30,37 @@ class Editor extends React.PureComponent<IProps> {
     return monaco.languages.typescript.getTypeScriptWorker().then(worker => worker(this.editor.getModel().uri))
   }
 
-  handleChange = (val: string) => {
-    this.props.onChange(this.props.currentFile, val)
-
+  handleChange = async (val: string): Promise<boolean | void> => {
+    let content = val
     if (this.props.language === EditorLanguage.TS) {
-      this.getTypescriptService()
-        .then(service => {
-          return service.getEmitOutput(this.editor.getModel().uri.toString())
-        })
-        .then((result: any) => {
-          if (result.emitSkipped) {
-            return false
-          }
+      const service = await this.getTypescriptService()
+      const result =  service.getEmitOutput(this.editor.getModel().uri.toString())
+      if (result.emitSkipped) {
+        return false
+      }
 
-          if (!result.outputFiles || !result.outputFiles[0]) {
-            return false
-          }
+      if (!result.outputFiles || !result.outputFiles[0]) {
+        return false
+      }
 
-          return result.outputFiles[0].text
-        })
-        .then(text => {
-          if (typeof text === 'string') {
-            console.log('transpiled:', text)
-          }
-        })
+      const text = await result.outputFiles[0].text
+      if (typeof text === 'string') {
+        console.log('transpiled:', text)
+        content = text
+      }
     }
+    this.setState({ value: content })
+    this.debouncedOnChange()
   }
 
   render() {
-    const { files, currentFile, addFiles, removeFiles, changeCurrentFile, language } = this.props
+    const { files, addFiles, removeFiles, changeCurrentFile, language } = this.props
+    const { value } = this.state
 
     return (
       <React.Fragment>
         <EditorTabs files={files} handlerAdd={addFiles} handlerRemove={removeFiles} handlerCurrentFile={changeCurrentFile} />
-        <Monaco language={language} className="editor-wrapper" value={files[currentFile].content} onEditorCreated={this.handleEditorCreated} onChange={this.handleChange} />
+        <Monaco language={language} className="editor-wrapper" value={value} onEditorCreated={this.handleEditorCreated} onChange={this.handleChange} />
       </React.Fragment>
     )
   }
--- src/modules/common/utils.ts
+++ src/modules/common/utils.ts
@@ -5,3 +5,18 @@ export function normalizeTypes(type): { request: string; success: string; failed
     failed: `[Failed] ${type}`
   }
 }
+
+export function debounce(func: any, wait: number, immediate?: boolean) {
+  let timeout
+  return function(this: any) {
+    const context = this, args = arguments
+    const later = function() {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    };
+    const callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
