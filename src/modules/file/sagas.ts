56ed7e8347e15c52fe7cb3b4276bbba4d60d59ab
belohlavek <dbhvk.96@gmail.com>
2018-06-09 15:55:34 -0300

    chore: updated preview

--- src/modules/file/sagas.ts
+++ src/modules/file/sagas.ts
@@ -1,22 +1,14 @@
 import { takeLatest, put } from 'redux-saga/effects'
 
-import { WRITE_FILE, writeFileSuccess } from './actions';
-
-
-declare global {
-  interface Window {
-    sceneJson: any,
-    reloadPreview: () => void
-  }
-}
+import { WRITE_FILE, writeFileSuccess } from './actions'
 
 export default function* fileSagas() {
   yield takeLatest(WRITE_FILE.request, handleWriteFile)
 }
 
 function* handleWriteFile({ name, content }: any) {
-  const blobUrl = 'data:text/plain;base64,' + btoa(content)
-  yield put(writeFileSuccess(name, content, blobUrl))
-  window.sceneJson.main = blobUrl
-  window.reloadPreview()
+  const encoded = 'data:text/plain;base64,' + btoa(content)
+  yield put(writeFileSuccess(name, content, encoded))
+  window['sceneJson'].main = encoded
+  window['handleServerMessage']({ type: 'update' })
 }
